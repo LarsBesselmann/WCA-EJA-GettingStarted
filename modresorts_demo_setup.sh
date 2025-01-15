15d93fec60e10acf876c9598dc168851431d229d
LarsBesselmann <Lars.Besselmann@mail.de>
2025-01-15 12:19:29 +0100

    Updated

---
+++ modresorts_demo_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NOW="$(date +"%m-%d-%Y-%H-%M")"
+DIR="temp-$NOW/modresorts"
+mkdir -p $DIR
+cd $DIR
+unzip ~/software/modresorts*.zip
+git init
+git config --global user.name "John Doe"
+$ git config --global user.email johndoe@example.com
+git add .
+git commit -a -m "Initial project"
