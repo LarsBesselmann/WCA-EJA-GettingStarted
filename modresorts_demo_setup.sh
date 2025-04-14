27f9d9a055f64e3fd82a68d0496a2c9fb44b2e95
LarsBesselmann <Lars.Besselmann@mail.de>
2025-04-14 17:02:26 +0200

    Updated

--- modresorts_demo_setup.sh
+++ modresorts_demo_setup.sh
@@ -3,7 +3,7 @@ NOW="$(date +"%m-%d-%Y-%H-%M")"
 DIR="temp-$NOW/modresorts"
 mkdir -p $DIR
 cd $DIR
-unzip ~/software/modresorts*.zip
+unzip ~/software/modresorts-twas*.zip
 git init
 git config --global user.name "John Doe"
 $ git config --global user.email johndoe@example.com
