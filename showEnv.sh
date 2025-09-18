e71fb3c9e8dc36baa500140618b28d26555d2658
Lars Besselmann <lars.besselmann@de.ibm.com>
2025-09-18 18:40:14 +0200

    Fix code

--- showEnv.sh
+++ showEnv.sh
@@ -30,17 +30,6 @@ do
 		code --version
 		break
 		;;
-	wca1)
-		wca_backend=WCAv10JHuPp-Ps5tN2YSaxSqcLTUTdeEgdofeuBL48hZ
-		echo ${wca_backend/WCA/$2}
-		break
-		;;
-	wca2)
-		wca_backend=WCAa0pqP8mw-8h82lMUMFXqZ0P32oNP-1Mp89cXg1Iu9
-		echo ${wca_backend/WCA/$2}
-		break
-		;;
-		
 		
 	*)
 		echo "Sorry, I don't understand"
